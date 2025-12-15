7c8044f4a9d611bd19fa802705e4c7502010cb22
Gokul <gokulravivcr05@gmail.com>
2025-12-15 23:23:49 +0530

    add filter btn

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,14 @@
 import RestaurantContainer from "./RestaurantContainer";
+import { useState, useEffect } from "react";
 
 const Body = () => {
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button className="filter-btn" onClick={() => {}}>
+          Top Rated
+        </button>
+      </div>
       <RestaurantContainer />
     </div>
   );
